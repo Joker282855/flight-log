c7a82d8ef8c793dbb0ec4830dbf37729e51a5eeb
unknown <jtjackson325@gmail.com>
2022-12-09 11:47:25 -0700

    encrypted the users sensitive information to the database

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,16 @@
-require('dotenv').config();
-
+// import the sequelize package
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
+require('dotenv').config();
+
+// create connection to the database
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    
+    host: 'localhost',
+
+    dialect: 'mysql',
+
+    port: 3306
+});
+
+module.exports = sequelize
