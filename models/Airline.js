02f62e99de8ce644a023759fca4133e5f150916d
unknown <jtjackson325@gmail.com>
2022-12-12 15:53:29 -0700

    made th module for the airline table

---
+++ models/Airline.js
@@ -0,0 +1,54 @@
+// name the model we are going to use
+const { Model, Datatypes, DataTypes } = require('sequelize');
+
+// import the library that will define our model
+const sequelize = require('../config/connection');
+
+// inital the model we are using
+class Airline extends Model {}
+
+// set up the parameters for our models information and initialize database
+Airline.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    airline_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.DECIMAL,
+        alowNull: false,
+        validate: {
+            isDecimal: true
+        }
+    },
+    how_many: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 15,
+        validate: {
+            isNumerical: true
+        }        
+    },
+        destination_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'destination',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: false,
+        underscored: true,
+        modelName: 'airline,'
+    }
+)
+
+module.exports = Airline;
