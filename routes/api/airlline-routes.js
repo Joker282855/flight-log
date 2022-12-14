0606a3d8708f05b1c3022fe446ad7bbf905780a4
unknown <jtjackson325@gmail.com>
2022-12-13 18:43:12 -0700

    added the get route for the Airline model

--- routes/api/airlline-routes.js
+++ routes/api/airlline-routes.js
@@ -1,20 +1,34 @@
 const router = requrie('express').Router();
 const { Airline } = require('../../models');
 
+// gets all of the information for the ariline
 router.get('/', (req, res) => {
+    Airline.findAll()
 
+        .then(dbAirlineData => res.json(dbAirlineData))
+        
+        .catch(err => {
+            console.log(err);
+
+            res.status(500).json(err);
+
+        })
 });
 
+// allows us to get information for a specific flight
 router.get('/:id', (req, res) => {
 
 });
 
+// allows us to add infomration into the Airline talbe
 router.post('/', (req, res) => {
 
 });
 
+// allows us to update existing information
 router.put('/:id', (req, res) => {
 
 });
 
+// allows us to delete unveccessary information
 router.delete('/:id');
