47fcfdd8c5703fa9a7fadecc2c9793a5bef6c6d7
unknown <jtjackson325@gmail.com>
2022-12-13 19:06:44 -0700

    added the informotion for the destroy route for the Airline  model

--- routes/api/airlline-routes.js
+++ routes/api/airlline-routes.js
@@ -3,6 +3,7 @@ const { Airline } = require('../../models');
 
 // gets all of the information for the ariline
 router.get('/', (req, res) => {
+
     Airline.findAll()
 
         .then(dbAirlineData => res.json(dbAirlineData))
@@ -18,6 +19,7 @@ router.get('/', (req, res) => {
 
 // allows us to get information for a specific flight
 router.get('/:id', (req, res) => {
+
     Airline.findOne({
         
         where: {
@@ -49,6 +51,7 @@ router.get('/:id', (req, res) => {
 
 // allows us to add infomration into the Airline talbe
 router.post('/', (req, res) => {
+
     Airline.create({
 
         ariline_name: req.body.airline_name,
@@ -73,7 +76,70 @@ router.post('/', (req, res) => {
 // allows us to update existing information
 router.put('/:id', (req, res) => {
 
+    Airline.update(req.body, {
+
+        where: {
+
+            id: req.params.id
+
+        }
+    })
+
+        .then(dbAirlineData => {
+
+            if (!dbAirlineData[0]) {
+
+                res.status(404).json({ message: 'No Airline found with this id' });
+
+                return;
+            }
+
+            res.json(dbAirlineData);
+
+        })
+
+            .catch(err => {
+
+                console.log(err);
+
+                res.status(500).json(err);
+
+            });
 });
 
 // allows us to delete unveccessary information
-router.delete('/:id');
+router.delete('/:id', (req, res) => {
+
+    Airline.destroy({
+
+        where: {
+
+            id: req.params.id
+
+        }
+    })
+
+        .then(dbAirlineData => {
+
+            if (!dbAirlineData) {
+
+                res.status(404).json({ message: 'No Airline found with this id' });
+
+                return;
+            }
+
+            res.json(dbAirlineData);
+
+        })
+
+            .catch(err => {
+
+                console.log(err);
+
+                res.status(500).json(err);
+
+            });
+
+});
+
+module.exports = router;
