0f9388967dcbf82e5f9d0338fc84f60e249c6cdc
unknown <jtjackson325@gmail.com>
2023-01-12 20:35:37 -0700

    added the weather model to the database

--- models/Weather.js
+++ models/Weather.js
@@ -4,15 +4,25 @@ const sequelize = require('../config/connection');
 
 class Weather extends Model {}
 
-Weather.init({
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+Weather.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        weather_condition: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
     },
-    weather_condition: {
-        type: DataTypes.STRING,
-        allowNull: true
+    {
+        sequelize,
+        freezeTableName: false,
+        underscored: true,
+        modelName: 'weather'
     }
-})
+)
+
+module.exports = Weather;
