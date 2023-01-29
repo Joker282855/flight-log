6dcc3b9384d0eda8fdb753c1cbc0fc9dc941aba0
unknown <jtjackson325@gmail.com>
2023-01-28 18:45:36 -0700

    started adding data to the seed files

---
+++ seeds/airline.js
@@ -0,0 +1,13 @@
+const { Airline } = require('../models');
+
+const airlineData = [
+    {
+        destination_name: 'Brazil, Brazil',
+    },
+    {
+        destination_name: 'England, London',
+    },
+    {
+        destination_name: ''
+    }
+]
