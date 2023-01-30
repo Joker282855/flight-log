c30f4db87fc08a7b0e6794b47ee84999d1d5f6d8
unknown <jtjackson325@gmail.com>
2023-01-30 14:05:40 -0700

    added more information to one of the seeds

--- seeds/destination.js
+++ seeds/destination.js
@@ -8,6 +8,33 @@ const destinationData = [
         airline_id: 1
     },
     {
-        airline_name: ''
+        airline_name: 'Southwest Airlines',
+        price: 344.69,
+        quantity: 8,
+        airline_id: 2
+    },
+    {
+        airline_name: 'Delta Airlines',
+        price: 144.58,
+        quantity: 4,
+        airline_id: 3
+    },
+    {
+        airline_name: 'Continential Airlines',
+        price: 245.79,
+        quantity: 5,
+        airline_id: 4
+    },
+    {
+        airline_name: 'Southwest Airlines',
+        price: 446.78,
+        quantity: 7,
+        airline_id: 5
+    },
+    {
+        airline_name: 'JetBlue Airlines',
+        price: 324.56,
+        quantity: 2,
+        airline_id: 6
     }
 ]
